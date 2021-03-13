173da2028150a2c9f4c41778fdd820da2376843d
PixelsDE <daniel.schoppe2000@gmail.com>
2021-03-13 10:44:59 +0100

    commit 2

--- src/main/java/de/bypixels/Utils/ClickGUI.java
+++ src/main/java/de/bypixels/Utils/ClickGUI.java
@@ -1,4 +1,35 @@
 package de.bypixels.Utils;
 
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+
 public class ClickGUI {
+
+    private Inventory inventory = Bukkit.createInventory(null, 9, "§dChallanges Inventory");
+
+
+    public void createInventory() {
+
+
+    }
+
+
+    private void fill(Inventory inv) {
+        ItemStack fill = new ItemStack(Material.GLASS_PANE, (byte) 3);
+        ItemMeta fillMeta = fill.getItemMeta();
+        fillMeta.setDisplayName("");
+        fillMeta.addEnchant(Enchantment.DURABILITY, 0, true);
+        fill.setItemMeta(fillMeta);
+        for (int slot = 0; slot <= inv.getSize(); slot++) {
+            if (inv.getItem(slot) != null)
+                inv.setItem(slot, fill);
+        }
+    }
 }
---
+++ src/main/java/de/bypixels/challanges/ChallangesGUI.java
@@ -0,0 +1,18 @@
+package de.bypixels.challanges;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class ChallangesGUI implements CommandExecutor {
+
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        if (command.getName().equalsIgnoreCase("")){
+
+        }
+        return false;
+    }
+}
